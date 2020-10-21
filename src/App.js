291fd2f94aadf115592f93ac9565bba43b8fa6e6
Lakendria <kendra939@gmail.com>
2020-10-20 20:23:41 -0400

    Set useState and use Effect, but still getting errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [date , setDate] = useState("");
+  const [img, setImg] = useState("");
+  const [ details, setDetails] = useState("");
+
+
+  useEffect(()=>{
+    axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=VStpaWrZdU6vCt0bpdlazaMGTpGwDUF4CqtaiaZF`)
+    .then(res => {
+      const image = res.data;
+      console.log(image);
+      setImg(image.hdurl);
+      setDate(image.date);
+      setTitle(image.title);
+      setDetails(image.details);
+    })
+
+
+  },[]);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h2>{date}</h2>
+      <h2>{title}</h2>
+      <img src = {img} alt ="potd"/>
+      <p>{details}</p>
     </div>
   );
 }
