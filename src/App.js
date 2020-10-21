e2634caf9e7a608708038dbedb0058563ab9c142
Lakendria <kendra939@gmail.com>
2020-10-20 21:44:06 -0400

    separtated into two components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React from "react";
+import CardList from './CardList';
+
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([])
- /* const [title, setTitle] = useState("");
-  const [date , setDate] = useState("");
-  const [img, setImg] = useState("");
-  const [ details, setDetails] = useState("");
-
-*/
-  useEffect(()=>{
-    axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-10-20`)
-    .then(res => {
-      console.log(res.data);
-      setData(res.data);  
-    })
-    .catch(err => {
-      console.log('err',err)
-    })
-  },[]);//dependency array
 
   return (
     <div className="App">
-      
+      <CardList/>
     </div>
   );
 }
